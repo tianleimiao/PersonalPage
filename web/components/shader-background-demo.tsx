c1ef6c0e97befb7ec0c60431b27502438143b3f4
Tianlei Miao <nbyzmtl@gmail.com>
2026-04-04 09:38:47 +0200

    remove unused demo

--- web/components/shader-background-demo.tsx
+++
@@ -1,9 +0,0 @@
-import { AnimatedShaderBackground } from "@/components/ui/animated-shader-background";
-
-export function ShaderBackgroundDemoOne() {
-  return (
-    <div className="h-screen w-full bg-black">
-      <AnimatedShaderBackground className="h-full w-full" />
-    </div>
-  );
-}
